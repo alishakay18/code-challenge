184ff2c4ea7f097b9f1bc3d68560bcefba95fc02
Alisha <alishakay18@mac.com>
2021-05-11 11:06:10 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -10,8 +10,46 @@ console.log(findSum([2,4,6]));
 
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+var most;
+var mostFreq = 1;
+var m = 0;
+for (var i=0; i<array.length; i++)
+{
+  for (var j=i; j<array.length; j++)
+        {
+           if (array[i] == array[j])
+                 m++;
+                if (mostFreq<m)
+                {
+                  mostFreq=m; 
+                  most = array[i];
+                }
+        }
+        m=0;
+}
+
+ let leastFreq = array[0]
+  const counted = array.reduce((acc, item) => { 
+    if (!acc[item]) {
+      acc[item] = 1
+    } else {
+      acc[item]++
+    }
+
+    if(acc[item] < acc[leastFreq]) {
+      leastFreq = item
+    }
+
+    return acc;
+  }, {});
+
+return ("most: " + most + ", least: " + leastFreq);
+
 };
+
+findFrequency(['a', 'b', 'c', 'a', 'b', 'c', 'a', 'a', 'd']);
+ 
+
 
 const isPalindrome = function(str) {
   // your code here - don't forget to return a boolean!
